833619e52b20155762a43d7738592c7ef32b3692
blanco <skrk309@naver.com>
2021-07-28 20:13:08 +0900

    start layout complete
    to do list
    
    1. kakao login
    2. make mainactivity,layout

--- app/src/main/java/com/example/finedustalarm/StartActivity.kt
+++ app/src/main/java/com/example/finedustalarm/StartActivity.kt
@@ -1,4 +1,36 @@
 package com.example.finedustalarm
 
-class StartActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.example.finedustalarm.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
+
+        binding.startTxt1.startAnimation(animation)
+
+        var handler = Handler()
+        handler.postDelayed( {
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }, 3000)
+    }
+
+    override fun onPause(){
+        super.onPause()
+        finish()
+    }
 }

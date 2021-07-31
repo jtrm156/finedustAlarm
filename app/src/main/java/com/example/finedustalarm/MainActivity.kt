9c7453f143431b37049a17ed0481403e7df0f277
blanco <skrk309@naver.com>
2021-07-31 19:09:57 +0900

    complete 1. kakao login 2. make mainactivity,layout
    to do list
    1. take information for finedust
    2. kakao map details complete

--- app/src/main/java/com/example/finedustalarm/MainActivity.kt
+++ app/src/main/java/com/example/finedustalarm/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.finedustalarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.viewpager2.widget.ViewPager2
 import com.example.finedustalarm.databinding.ActivityMainBinding
+import com.kakao.sdk.user.UserApiClient
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.mainMap.setOnClickListener()
+        {
+            var intent = Intent(this, mapactivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+
+        UserApiClient.instance.logout { error ->
+            if (error != null) {
+                Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
+            }else {
+                //Toast.makeText(this, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/finedustalarm/StartActivity.kt
+++ app/src/main/java/com/example/finedustalarm/StartActivity.kt
@@ -1,11 +1,21 @@
 package com.example.finedustalarm
 
+import android.content.ContentValues.TAG
+import com.kakao.sdk.common.util.Utility
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.finedustalarm.databinding.ActivityStartBinding
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthErrorCause
+import com.kakao.sdk.common.model.AuthErrorCause.*
+import com.kakao.sdk.user.UserApiClient
 
 class StartActivity : AppCompatActivity() {
 
@@ -17,20 +27,86 @@ class StartActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        /*
+        var keyHash = Utility.getKeyHash(this)
+        Log.v(TAG, keyHash)
+        */
+
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                when {
+                    error.toString() == AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else if (token != null) {
+                //Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this,MainActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            }
+        }
+
         val animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
 
         binding.startTxt1.startAnimation(animation)
 
+        binding.kakao.setOnClickListener {
+            LoginClient.instance.run {
+                if (isKakaoTalkLoginAvailable(this@StartActivity)) {
+                    loginWithKakaoTalk(this@StartActivity, callback = callback)
+                } else {
+                    loginWithKakaoAccount(this@StartActivity, callback = callback)
+                }
+            }
+        }
+
         var handler = Handler()
         handler.postDelayed( {
-            var intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-
-        }, 3000)
+        }, 5000)
     }
 
-    override fun onPause(){
-        super.onPause()
+    override fun onStop(){
+        super.onStop()
+
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                //Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            }
+            else if (tokenInfo != null) {
+                //Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                //val intent = Intent(this, MainActivity::class.java)
+                //startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            }
+        }
         finish()
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
     }
 }
